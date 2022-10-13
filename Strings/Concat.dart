545624d2ea4d232eb3a47110ce07ee6f48249040
Johar M. Ashfaque, Ph.D., MInstP, AATQB <39216339+UKVeteran@users.noreply.github.com>
2022-10-13 20:02:05 +0100

    Add files via upload

---
+++ Strings/Concat.dart
@@ -0,0 +1,7 @@
+void main() {   
+String firstName = "John";
+String lastName = "Doe";
+print("Using +, Full Name is "+firstName + " " + lastName+".");
+print("Using interpolation, fulname is $firstName $lastName.");  
+  
+}
